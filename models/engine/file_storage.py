691e68cdb09290a2f5fabbd96d0b205a5a0829bc
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-03-04 23:19:22 +0000

    add tests and comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,62 @@
 #!/usr/bin/python3
 """
-This module serializes instances to a JSON file
-and deserializes JSON file to instances
+module that contain classes
+
+Class:
+    FileStorage : serializes instances to a JSON file
+        and deserializes JSON file to instances
 """
 
-from os.path import exists
-from models.base_model import BaseModel
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.amenity import Amenity
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
     """
-    This class creates a FileStorage Instance
+    serializes instances to a JSON file and deserializes JSON file to instances
+
+    Functions:
+        all : returns the dictionary __objects
+        new : sets in __objects the obj with key <obj class name>.id
+        save : serializes __objects to the JSON file
+        reload : deserializes the JSON file to __objects (only if the JSON file
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ This function returns the dictionary __objects"""
+        """
+        returns the dictionary __objects
+
+        Return:
+            the dictionary __objects
+        """
         return self.__objects
 
     def new(self, obj):
         """
-        This function sets in __objects the obj with
-        key <obj class name>.id
+        sets in __objects the obj with key <obj class name>.id
+
+        Arguments:
+            obj : object to set in __objects
+
+        Returns:
+            None
         """
         self.__objects.update({f"{obj.__class__.__name__}.{obj.id}": obj})
 
     def save(self):
         """
-        This function serializes __objects to
-        the JSON file (path: __file_path)
+        serializes __objects to the JSON file
+
+        Return:
+            None
         """
         dict = {}
         for key, value in self.__objects.items():
